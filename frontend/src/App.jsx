3377d373c3a5736156ec792628fa9e8fe3d8b330
Nakshishah2003 <nakshishah2003@gmail.com>
2025-01-24 13:42:43 +0530

    Added form.jsx form.css and modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
 import AppRoutes from "./routes";
+import { createTheme, ThemeProvider } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#c2185b",
+      light: "#ec407a",
+      dark: "#e91e63",
+    },
+  },
+});
 function App() {
   return (
     <>
-      <AppRoutes />
+      {/* ThemeProvider applies the custom theme globally to all MUI components within its child hierachy */}
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
     </>
   );
 }
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import "./form.css";
+//import { useState } from "react";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Input from "./Input";
-import Button from "./Button";
+import { useNavigate, useLocation } from "react-router-dom";
+import { Button, TextField } from "@mui/material";
 const Form = () => {
   const navigate = useNavigate();
-
+  const location = useLocation();
   const [isSignInPage, setIsSignInPage] = useState(true);
-  const [isTransitioning, setIsTransitioning] = useState(false);
   useEffect(() => {
     const path = location.pathname;
     if (path.includes("signup")) {
@@ -15,8 +14,7 @@ const Form = () => {
     } else {
       setIsSignInPage(true);
     }
-  }, []);
-
+  }, [location]);
   const [formData, setformData] = useState({
     ...(!isSignInPage && {
       firstName: "",
@@ -58,6 +56,7 @@ const Form = () => {
         navigate("/user/dashboard");
       } else if (response.status == 201 && !isSignInPage) {
         alert(`${formData.firstName} you have registered successfully`);
+        console.log("login");
         navigate("/account/login");
       } else {
         const message = data.message;
@@ -69,139 +68,132 @@ const Form = () => {
   };
 
   const handleTogglePage = () => {
-    setIsTransitioning(true);
     setTimeout(() => {
       setIsSignInPage((prev) => !prev);
-      setIsTransitioning(false);
       navigate(`${isSignInPage ? "/account/signup" : "/account/login"}`);
     }, 300);
   };
   return (
-    <div
-      className={`h-screen font-inter flex flex-col transition-all duration-500 ease-in-out ${
-        isSignInPage ? "lg:flex-row" : "lg:flex-row-reverse"
-      } ${isTransitioning ? "bg-black" : ""}`}
-    >
+    <div className="flex-create">
       {isSignInPage ? (
         <>
-          {/* Left semicircle div */}
-          <div
-            className={`bg-black lg:w-[50%] w-full h-[90%] lg:h-full lg:rounded-br-full lg:rounded-tr-[3rem] rounded-bl-lg shadow-md shadow-slate-800 flex justify-center items-center transition-all duration-1000 ease-in-out`}
-          >
-            <div className="text-white text-center h-1/4 text-xl lg:text-4xl transition-all duration-500 ease-in-out">
-              Welcome to Camconnect
-            </div>
-          </div>
-          {/* Div for signin */}
-          <div
-            className={`relative lg:w-[50%] w-full h-screen lg:h-full flex flex-col lg:justify-center items-center -mt-10 transition-all duration-300 ease-in-out`}
-          >
-            <form
-              className={`bg-gray-600 w-full h-full lg:w-[70%] lg:h-[50%] shadow-lg shadow-gray-700 lg:rounded-[3rem] rounded-tr-[2rem] rounded-tl-[2rem] transition-all duration-300 ease-in-out`}
-              onSubmit={handleSubmit}
-            >
-              <Input
-                type="text"
-                name="username"
-                placeholder="Username"
-                className="mt-10"
-                onChange={handleChange}
-                value={formData.username}
-              />
-              <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={handleChange}
-                value={formData.password}
-              />
-              <Button name="Sign in" className="h-[13%]" type="submit" />
-              <p className="font-bold text-center mt-6 text-sm text-white">
-                Forgot password?
-              </p>
-              <p
-                className="font-bold text-center mt-5 text-sm text-white cursor-pointer"
-                onClick={handleTogglePage}
-              >
-                Don&apost have an account? Sign up here
-              </p>
-            </form>
-          </div>
+          <h2 className="text"> Welcome To Camconnect</h2>
+
+          <form className="flex-div-child-node" onSubmit={handleSubmit}>
+            <h2 className="text">Login</h2>
+            <TextField
+              type="text"
+              name="username"
+              id="standard-basic"
+              label="Username"
+              variant="filled"
+              className="textField"
+              color="primary"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            ></TextField>
+
+            <TextField
+              type="password"
+              name="password"
+              id="standard-basic"
+              label="Password"
+              variant="filled"
+              className="textField"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            ></TextField>
+
+            <Button variant="contained" className="button" type="submit">
+              Login
+            </Button>
+            <p>Forgot password?</p>
+            <p onClick={handleTogglePage} className="navigate">
+              Don&apos;t have an account? Sign up here
+            </p>
+          </form>
         </>
       ) : (
         <>
-          {/* Right semicircle div */}
-          <div className="bg-black lg:w-[50%] w-full h-full lg:h-full lg:rounded-tl-full lg:rounded-tr-[3rem] rounded-bl-lg shadow-md shadow-slate-800 flex lg:justify-center justify-evenly lg:items-center items-start transition-all duration-500 ease-in-out">
-            <div className="text-white  lg:text-center h-1/4 text-xl lg:text-4xl mt-9 transition-all duration-300 ease-in-out">
-              Signup to connect
-            </div>
-          </div>
-          {/* Div for signup */}
-          <div
-            className={`lg:w-[50%] w-full h-screen lg:h-full flex flex-col justify-evenly items-center -mt-96 lg:mt-0.5 transition-all duration-300 ease-in-out`}
-          >
-            <form
-              className="bg-gray-600 w-full h-full lg:w-[70%] lg:h-[80%] shadow-lg shadow-gray-700 lg:rounded-[3rem] rounded-tr-[2rem] rounded-tl-[2rem] flex flex-col justify-evenly transition-all duration-500 ease-in-out"
-              onSubmit={handleSubmit}
-            >
-              <Input
-                type="text"
-                name="firstName"
-                placeholder="Firstname"
-                className="lg:h-[10%] lg:mt-10 mt-7"
-                value={formData.firstName}
-                onChange={handleChange}
-              />
-              <Input
-                type="text"
-                name="lastName"
-                placeholder="Lastname"
-                className="lg:h-[10%]"
-                value={formData.lastName}
-                onChange={handleChange}
-              />
-              <Input
-                type="text"
-                name="username"
-                placeholder="Username"
-                className="lg:h-[10%]"
-                value={formData.username}
-                onChange={handleChange}
-              />
-              <Input
-                type="date"
-                name="dateOfBirth"
-                placeholder="mm/dd/yyyy"
-                className="lg:h-[10%]"
-                value={formData.dateOfBirth}
-                onChange={handleChange}
-              />
-              <Input
-                type="email"
-                name="emailId"
-                placeholder="Email"
-                className="lg:h-[10%]"
-                value={formData.emailId}
-                onChange={handleChange}
-              />
-              <Input
-                type="password"
-                name="password"
-                placeholder="Password"
-                className="lg:h-[10%]"
-                value={formData.password}
-                onChange={handleChange}
-              />
-              <Button name="Sign up" className="mt-2 h-[13%]" />
+          {/*-----------Signup page -------------*/}
+          <form className="flex-div-child-node" onSubmit={handleSubmit}>
+            <h2 className="text">Signup</h2>
+            <TextField
+              type="text"
+              name="firstName"
+              id="standard-basic"
+              label="Firstname"
+              variant="filled"
+              className="textField"
+              color="primary"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            ></TextField>
 
-              <p
-                className="text-center mt-2 text-sm text-white mb-3 cursor-pointer"
-                onClick={handleTogglePage}
-              >
-                Already have an account?<b> Sign in here</b>
-              </p>
-            </form>
-          </div>
+            <TextField
+              type="text"
+              name="lastName"
+              id="standard-basic"
+              label="Lastname"
+              variant="filled"
+              className="textField"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            ></TextField>
+            <TextField
+              type="text"
+              name="username"
+              id="standard-basic"
+              label="Username"
+              variant="filled"
+              className="textField"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            ></TextField>
+            <TextField
+              type="date"
+              name="dateOfBirth"
+              id="standard-basic"
+              variant="filled"
+              className="textField"
+              value={formData.dateOfBirth}
+              onChange={handleChange}
+              required
+            ></TextField>
+            <TextField
+              type="email"
+              name="emailId"
+              id="standard-basic"
+              variant="filled"
+              className="textField"
+              label="Email"
+              value={formData.emailId}
+              onChange={handleChange}
+              required
+            ></TextField>
+            <TextField
+              type="password"
+              name="password"
+              id="standard-basic"
+              variant="filled"
+              className="textField"
+              label="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            ></TextField>
+            <Button variant="contained" className="button" type="submit">
+              Signup
+            </Button>
+            <p onClick={handleTogglePage} className="navigate">
+              Already have an account?<b> Sign in here</b>
+            </p>
+          </form>
         </>
       )}
     </div>

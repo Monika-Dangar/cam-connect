0b749b0049021c68aaaf14d4e2a37d68e2e45741
Monika Dangar <dangarmonika191023@gmail.com>
2025-01-31 12:32:29 +0530

    Toast component added to handle error for user

--- frontend/src/components/toaster/TransitionsSnackbar.jsx
+++ frontend/src/components/toaster/TransitionsSnackbar.jsx
@@ -1,124 +1,16 @@
-// import * as React from 'react';
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import Button from '@mui/material/Button';
-// import Snackbar from '@mui/material/Snackbar';
+import React from 'react';
+import { Snackbar, Slide } from '@mui/material';
 
-// export default function PositionedSnackbar() {
-//   const [state, setState] = React.useState({
-//     open: false,
-//     vertical: 'top',
-//     horizontal: 'center',
-//   });
-//   const { vertical, horizontal, open } = state;
-
-//   const handleClick = (newState) => () => {
-//     setState({ ...newState, open: true });
-//   };
-
-//   const handleClose = () => {
-//     setState({ ...state, open: false });
-//   };
-
-//   const buttons = (
-//     <React.Fragment>
-//       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-//         <Button onClick={handleClick({ vertical: 'top', horizontal: 'center' })}>
-//           Top-Center
-//         </Button>
-//       </Box>
-//       <Grid container sx={{ justifyContent: 'center' }}>
-//         <Grid item xs={6}>
-//           <Button onClick={handleClick({ vertical: 'top', horizontal: 'left' })}>
-//             Top-Left
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6} sx={{ textAlign: 'right' }}>
-//           <Button onClick={handleClick({ vertical: 'top', horizontal: 'right' })}>
-//             Top-Right
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6}>
-//           <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'left' })}>
-//             Bottom-Left
-//           </Button>
-//         </Grid>
-//         <Grid item xs={6} sx={{ textAlign: 'right' }}>
-//           <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'right' })}>
-//             Bottom-Right
-//           </Button>
-//         </Grid>
-//       </Grid>
-//       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-//         <Button onClick={handleClick({ vertical: 'bottom', horizontal: 'center' })}>
-//           Bottom-Center
-//         </Button>
-//       </Box>
-//     </React.Fragment>
-//   );
-
-//   return (
-//     <Box sx={{ width: 500 }}>
-//       {buttons}
-//       <Snackbar
-//         anchorOrigin={{ vertical, horizontal }}
-//         open={open}
-//         onClose={handleClose}
-//         message="I love snacks"
-//         key={vertical + horizontal}
-//       />
-//     </Box>
-//   );
-// }
-
-import * as React from 'react';
-// import Button from '@mui/material/Button';
-import Snackbar from '@mui/material/Snackbar';
-import Fade from '@mui/material/Fade';
-import Slide from '@mui/material/Slide';
-import Grow from '@mui/material/Grow';
-
-function SlideTransition(props) {
-  return <Slide {...props} direction="up" />;
-}
-
-function GrowTransition(props) {
-  return <Grow {...props} />;
-}
-
-export default function TransitionsSnackbar() {
-  const [state, setState] = React.useState({
-    open: false,
-    Transition: Fade,
-  });
-
-  const handleClick = (Transition) => () => {
-    setState({
-      open: true,
-      Transition,
-    });
-  };
-
-  const handleClose = () => {
-    setState({
-      ...state,
-      open: false,
-    });
-  };
-
+function TransitionsSnackbar({ open, message, onClose, autoHideDuration }) {
   return (
-    <div>
-      {/* <Button onClick={handleClick(GrowTransition)}>Grow Transition</Button>
-      <Button onClick={handleClick(Fade)}>Fade Transition</Button>
-      <Button onClick={handleClick(SlideTransition)}>Slide Transition</Button> */}
-      <Snackbar
-        open={state.open}
-        onClose={handleClose}
-        TransitionComponent={state.Transition}
-        message="I love snacks"
-        key={state.Transition.name}
-        autoHideDuration={1200}
-      />
-    </div>
+    <Snackbar
+      open={open}
+      onClose={onClose}
+      autoHideDuration={autoHideDuration}
+      TransitionComponent={(props) => <Slide {...props} direction="up" />}
+      message={message}
+    />
   );
 }
+
+export default TransitionsSnackbar;

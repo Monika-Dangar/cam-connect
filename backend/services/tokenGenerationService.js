2bbfaa94a04aa7b10b92465e9c1459f048ff48b1
Monika Dangar <dangarmonika191023@gmail.com>
2025-02-05 14:49:30 +0530

    Accessing response object wihtout having it

--- backend/services/tokenGenerationService.js
+++ backend/services/tokenGenerationService.js
@@ -1,6 +1,6 @@
-const { StatusCodes } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
-const { default: messages } = require("../utils/constants");
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const { default: messages } = require('../utils/constants');
 const secret = process.env.TOKEN_SECRET_KEY;
 
 function setUser(user) {
@@ -10,9 +10,7 @@ function setUser(user) {
 function getUser(token) {
   return jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .send({ message: messages.tokenNotVerified });
+      return { message: messages.tokenNotVerified };
     } else {
       return decoded;
     }

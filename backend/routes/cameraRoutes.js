b62c6efd4ed276a9305fc83b683d231482785aec
Monika Dangar <dangarmonika191023@gmail.com>
2025-02-10 17:42:39 +0530

    Created new route for denying access to device

--- backend/routes/cameraRoutes.js
+++ backend/routes/cameraRoutes.js
@@ -3,28 +3,14 @@ const router = express.Router();
 const { authenticate } = require('../services/authService');
 const camera = require('../controllers/cameraController');
 
-// router.route('/approvedRequest').get(camera.getApprovedDevice).post(camera.deniedAccessToDevice);
+router.route('/approvedRequest').get(authenticate, camera.getApprovedDevice);
 
-// router
-//   .route('/pendingRequest')
-//   .get(camera.getPendingRequest)
-//   .put(camera.allowAccessToDevice)
-//   .post(camera.deniedAccessToDevice);
-
-// router.route('/notificationBar').get(camera.getDeniedDevice).delete(camera.removeDeniedRequest);
-
-// router.route('/searchBar').post(camera.requestToAccessDevice).get(camera.findDevicesByUsername);
-
-router
-  .route('/approvedRequest')
-  .get(authenticate, camera.getApprovedDevice)
-  .post(authenticate, camera.deniedAccessToDevice);
+router.route('denyAccess').post(authenticate, camera.deniedAccessToDevice);
 
 router
   .route('/pendingRequest')
   .get(authenticate, camera.getPendingRequest)
-  .put(authenticate, camera.allowAccessToDevice)
-  .post(authenticate, camera.deniedAccessToDevice);
+  .put(authenticate, camera.allowAccessToDevice);
 
 router
   .route('/notificationBar')
